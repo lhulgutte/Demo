f6a1dba487e4d9d48ea892397d303f427a41931d
lhulgutte <125935046+lhulgutte@users.noreply.github.com>
2023-02-26 17:37:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function registerUser()
+const name=document.getElementById('name').value;
+console.log(name);
+const email=document.getElementById('email').value;
+console.log(email);
+const phone=document.getElementById('phone').value;
+console.log(phone);
+const Timeforcall=document.getElementById('Time for call').value;
+console.log(Timeforcall);
